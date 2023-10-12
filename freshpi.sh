d21e102cb75adbb6fdea342ac963aea26f5db672
Brandon Lindsey <bmlindsey@gmail.com>
2023-10-11 17:59:11 -0700

    Add success and failure messages

--- freshpi.sh
+++ freshpi.sh
@@ -8,12 +8,28 @@ setup
 
 # Update package lists
 info "Updating package lists"
-apt-get update
+if apt-get update; then
+    success "Successfully updated package lists"
+else
+    failure "Failed to update package lists"
+    exit 1
+fi
+
 
 # Upgrade installed packages
 info "Upgrading installed packages"
-apt-get -y -q upgrade
+if apt-get -y -q upgrade; then
+    success "Successfully upgraded installed packkages "
+else
+    failure "Failed to upgrade installed packages"
+    exit 1
+fi
 
 # Remove unnecessary packages
 info "Removing unnecessary packages"
-apt-get -y -q autoremove
+if apt-get -y -q autoremove; then
+    success "Successfully removed unnecessary packages"
+    exit 1
+else
+    failure "Failed to removed unnecessary packages"
+fi
