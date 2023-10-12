0c454fad7079ebf2120ae74949410b4372bb343d
Brandon Lindsey <bmlindsey@gmail.com>
2023-10-11 20:05:44 -0700

    Add check for existance of config file

--- freshpi_utils.sh
+++ freshpi_utils.sh
@@ -7,6 +7,17 @@ setup() {
     exec 3>&1
     exec 1>>${LOG_PATH}
     exec 2>&1
+
+    check_file_exists "config.conf"
+}
+
+# Check for the existance of a file
+check_file_exists() {
+    local file="$1"
+    if [ ! -f "$file" ]; then
+        failure "ERROR: ${file} does not exist!"
+        exit 1
+    fi
 }
 
 # Display an information message
@@ -24,7 +35,7 @@ failure() {
     echo -e "[${FAILURE_COLOR}x${RESET}] $1" | tee /dev/fd/3
 }
 
-# Display a failure message
+# Display a warning message
 warning() {
-    echo -e "[${WARNING_COLOR}x${RESET}] $1" | tee /dev/fd/3
+    echo -e "[${WARNING_COLOR}w${RESET}] $1" | tee /dev/fd/3
 }
