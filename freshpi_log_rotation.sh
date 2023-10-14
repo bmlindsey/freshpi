c4b7011b1aebe1145f6ba27c01dcb11f2f49e633
Brandon Lindsey <bmlindsey@gmail.com>
2023-10-14 15:33:56 -0700

    Add basic log rotation

--- freshpi_log_rotation.sh
+++ freshpi_log_rotation.sh
@@ -6,4 +6,21 @@ rename_current_log() {
     mv "${LOG_PATH}" "${LOG_PATH}.$(date +${LOG_DATE_FORMAT})"
 }
 
-rename_current_log
+file_exists () {
+    local file_path="$1"
+    if [[ -f "${file_path}" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+log_rotation () {
+    if file_exists "${LOG_PATH}"; then
+        rename_current_log
+    else
+        echo "${LOG_FILE} does not exist"
+    fi
+}
+
+log_rotation
