d388bc1c186237dabcd377be6cd30a5c1a1e8d88
Brandon <bmlindsey@gmail.com>
2023-10-30 10:27:45 -0700

    Add privelege check

--- freshpi.sh
+++ freshpi.sh
@@ -6,6 +6,12 @@ FRESHPI_UTILS_DIR="/opt/freshpi"
 source "${FRESHPI_VARS_DIR}/config.conf"
 source "${FRESHPI_UTILS_DIR}/freshpi_utils.sh"
 
+# Check for root privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "Please run this script with sudo."
+   exit 1
+fi
+
 # Setup environment
 setup
 
