8eabb65d28875e02a49de27cb5d2a218bec539cd
Brandon <bmlindsey@gmail.com>
2023-10-30 16:29:33 -0700

    Implement command line interface

--- bin/freshpi.sh
+++ bin/freshpi.sh
@@ -3,6 +3,7 @@
 source "/etc/freshpi/config.conf"
 source "/opt/freshpi/freshpi_utils.sh"
 source "/opt/freshpi/freshpi_upgrade_procedures.sh"
+source "/opt/freshpi/freshpi_log_rotation.sh"
 
 # Check for root privileges
 checkForRoot
@@ -13,10 +14,28 @@ ensureLogDirExists
 # Setup environment
 setupLogStreams
 
-# Run upgrades
-startupNotification
-updatePackageLists
-upgradeInstalledPackages
-removeUnnecessaryPackages
-cleanLocalRepository
-completionNotification
+# Display help/usage information
+display_help() {
+    {
+        echo "Usage: $(basename $0) [options]"
+        echo "Options:"
+        echo "  (no args)      Update your Raspberry Pi"
+        echo "  --rotate-logs  Rotate the logs."
+        echo "  --help         Display this help message."
+    } >&3
+    exit 1
+}
+
+# Check arguments
+if [[ "$1" == "--help" ]]; then
+    display_help
+elif [[ "$1" == "--rotate-logs" ]]; then
+    log_rotation
+else
+    startupNotification
+    updatePackageLists
+    upgradeInstalledPackages
+    removeUnnecessaryPackages
+    cleanLocalRepository
+    completionNotification
+fi
